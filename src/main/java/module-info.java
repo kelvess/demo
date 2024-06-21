ab0bc81775acc71c613a67946b19c099563d08e6
kelvess <91387670+kelvess@users.noreply.github.com>
2024-06-21 16:43:34 +0300

    first ver

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module org.example.demo1 {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.kordamp.bootstrapfx.core;
     requires java.desktop;
 
     opens org.example.demo1 to javafx.fxml;
